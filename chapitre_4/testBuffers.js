f2c0dd98e88b772f930b2ea7dd37c067f2704cbb
christophe <christoph92@yahoo.fr>
2016-04-22 22:36:24 +0200

    Essais Buffer

--- chapitre_4/testBuffers.js
+++ chapitre_4/testBuffers.js
@@ -4,3 +4,12 @@ console.log(buf.length);
 
 buf = new Buffer(7);
 console.log(buf);
+console.log("");
+
+buf = new Buffer(10);
+console.log(buf);
+console.log(buf.toString());
+
+buf.write("Bonjour", 1, 3);
+console.log(buf);
+console.log(buf.toString());
