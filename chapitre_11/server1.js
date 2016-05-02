e428eb99f5b7d4aed8d005b77f1e51cceddf7498
christophe <christoph92@yahoo.fr>
2016-05-02 10:49:53 +0200

    Fin socket.io

--- chapitre_11/server1.js
+++ chapitre_11/server1.js
@@ -11,11 +11,23 @@ server.listen(3000);
 
 var io = require("socket.io").listen(server);
 
-io.sockets.on("connection", function(socket)
+//io.sockets.on("connection", function(socket)
+io.of("/messages").on("connection", function(socket) // Connexions sur le namespace "/messages"
 {
    console.log("Un client s'est connecté");
+   var nom, prenom;
+   
    socket.on("event1", function(data)
    {
-      console.log(data);
+      console.log(data.prenom + " " + data.nom + " s'est connecté");
+      nom = data.nom;
+      prenom = data.prenom;
    });
+   
+   socket.on("disconnect", function()
+   {
+      console.log(prenom + " " + nom + " s'est déconnecté");
+   });
+   
+   socket.emit("event2", "Message envoyé du serveur vers le client");
 });
