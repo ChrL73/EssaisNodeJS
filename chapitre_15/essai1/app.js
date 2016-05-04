8c8da4cdfcfba56d90ba345cfa1d8cf2462b7ee1
christophe <christoph92@yahoo.fr>
2016-05-04 13:36:10 +0200

    Suite routage

--- chapitre_15/essai1/app.js
+++ chapitre_15/essai1/app.js
@@ -1,21 +1,52 @@
 var express = require('express');
 var routes = require('./routes');
+var logger = require("morgan");
+var util = require('util');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(logger("dev"));
+
 app.get('/', routes.index);
+
+app.all("/testAll", function(req, res, next)
+{
+   res.end("<p>Test de app.all</p>");
+});
+
+app.get(/ac/, function(req, res)
+{
+   res.end("<p>/ac/</p>")
+});
+
+app.get("/clients/:id", function(req, res)
+{
+   res.end("Valeur de :id = " + req.params.id);
+});
 
 app.get("/admin", function(req, res, next)
 {
-   res.write("<h2>Bonjour !</h2>");
+   res.write("<p>Bonjour !</p>");
    next();
 });
 
 app.get("/admin", function(req, res)
 {
-   res.end("<h3>Bienvenue dans la partie administration du site</h3>");
+   var html = "";
+   // 'app.routes' (indiqué dans le livre) ne fonctionne plus.
+   // Utiliser 'app._route.stack' à la place, qui contient des "layer"
+   // (un "layer" ne correspond pas forcément à une route)
+   app._router.stack.forEach(function(layer)
+   {
+      if(layer.route) html += "<p>" + util.inspect(layer.route) + "</p>";
+   });
+   res.end(html);
+   
+   //res.end("<h3>Bienvenue dans la partie administration du site</h3>");
 });
 
-app.listen(3000);
+console.log(util.inspect(app._router.stack));
+
+app.listen(3000)
---
+++ chapitre_15/essai2/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var logger = require("morgan");
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(logger("dev"));
+
+app.use("/clients", function use1(req, res, next)
+{
+   console.log("use1");
+   next();
+   console.log("use1 fin");
+});
+
+app.use("/clients", function use2(req, res, next)
+{
+   console.log("use2");
+   next();
+   console.log("use2 fin");
+});
+
+app.get("/clients", function getClients(req, res, next)
+{
+   console.log("get");
+   res.end("GET /clients OK");
+   console.log("get fin");
+});
+
+app.listen(3000)
