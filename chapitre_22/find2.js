43edfbd4ee5b34c7f02ecee2c55f15c4550f6be0
christophe <christoph92@yahoo.fr>
2016-05-15 22:47:40 +0200

    mongoose find

---
+++ chapitre_22/find2.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var db = mongoose.connect("mongodb://localhost/mydb2");
+
+var clientSchema = mongoose.Schema(
+{
+   nom: String,
+   prenom: String,
+   adresse: { rue: String, ville: String, cp: String }
+});
+
+var Client = mongoose.model("Client", clientSchema);
+
+Client.find().where("nom").gt("N").sort("nom").exec(function(err, clients)
+{
+   if (err) console.error(err);
+   else console.log(clients);
+});
