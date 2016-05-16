3e85fb733403b77073321c68e97c7080c3d58183
christophe <christoph92@yahoo.fr>
2016-05-16 09:10:58 +0200

    mongoose find fin

--- chapitre_22/find2.js
+++ chapitre_22/find2.js
@@ -10,8 +10,29 @@ var clientSchema = mongoose.Schema(
 
 var Client = mongoose.model("Client", clientSchema);
 
-Client.find().where("nom").gt("N").sort("nom").exec(function(err, clients)
+/*Client.find().where("nom").gt("N").sort("nom").exec(function(err, clients)
 {
    if (err) console.error(err);
    else console.log(clients);
+});*/
+
+/*Client.findOne().where("nom").gt("N").sort("nom").exec(function(err, client)
+{
+   if (err) console.error(err);
+   else console.log(client);
+});*/
+
+/*Client.count({ nom: /Obama/ }, function(err, count)
+{
+   console.log("Nombre de clients dont le nom contient 'Obama' : " + count);
+});*/
+
+/*Client.find({ nom: /Obama/ }).count(function(err, count)
+{
+   console.log("Nombre de clients dont le nom contient 'Obama' : " + count);
+});*/
+
+Client.find().where("nom").regex(/Obama/).count(function(err, count)
+{
+   console.log("Nombre de clients dont le nom contient 'Obama' : " + count);
 });
