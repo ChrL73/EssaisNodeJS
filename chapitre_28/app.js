50a0d7f36d5c6912d28236a3e14fe6ad168b9997
christophe <christoph92@yahoo.fr>
2016-05-24 15:31:11 +0200

    exemple appli suite

--- chapitre_28/app.js
+++ chapitre_28/app.js
@@ -21,6 +21,59 @@ app.get("/clients", function(req, res)
    });
 });
 
+app.get("/clients/voir/:id", function(req, res)
+{
+   var id = req.params.id;
+   Client.findById(id, function(err, client)
+   {
+      res.render("show.ejs", { client: client });
+   });
+});
+
+app.get("/clients/modifier/:id", function(req, res)
+{
+   var id = req.params.id;
+   Client.findById(id, function(err, client)
+   {
+      res.render("edit.ejs", { client: client });
+   });
+});
+
+app.get("/clients/supprimer/:id", function(req, res)
+{
+   var id = req.params.id;
+   Client.findByIdAndRemove(id, function(err, client)
+   {
+      res.redirect("/clients");
+   });
+});
+
+app.get("/clients/valider/:id?", function(req, res)
+{
+   var id = req.params.id;
+   var nom = req.query["nom"]; // req.param("nom") utilisé dans le livre est deprecated
+   var prenom = req.query["prenom"];
+   if (id)
+   {
+      Client.findByIdAndUpdate(id, { nom: nom, prenom: prenom }, function(err, client)
+      {
+         res.redirect("/clients");
+      });
+   }
+   else
+   {
+      Client.create({ nom: nom, prenom: prenom }, function(err, client)
+      {
+         res.redirect("/clients");
+      });
+   }
+});
+
+app.get("/clients/new", function(req, res)
+{
+   res.render("new.ejs");
+});
+
 app.listen(3000, function()
 {
    console.log("Server listening on port 3000...");
